89bb50c26f5664ae13b9cc4d1213d9547d36cf1c
Mr.Stijket <mr.stijket@gmail.com>
2023-06-24 17:40:11 +0300

    2/3 finished

---
+++ controllers/products.js
@@ -0,0 +1,38 @@
+const Product = require('../models/product');
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({}).select('name price').limit(5);
+  res.status(200).json({ msg: products, nbHits: products.length });
+}
+
+const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort, field } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  }
+  else {
+    result = result.sort('createdAt');
+  }
+
+
+  if (field) {
+    const fields = field.split(',').join(' ');
+    result = result.select(fields);
+  }
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
+}
+
+module.exports = { getAllProducts, getAllProductsStatic }
